fd4d3826a74aed6081e0a44b8dc5e684321cfac8
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-04-25 12:30:16 +0200

    * adds Configuration and mode

---
+++ custom-build-configuration/Configuration.swift
@@ -0,0 +1,22 @@
+//
+//  Configuration.swift
+//  custom-build-configuration
+//
+//  Created by Mathias Remshardt on 25.04.21.
+//
+import Foundation
+
+enum ApplicationMode {
+  case release
+  case test
+  case debug
+}
+
+
+#if TEST
+let mode = ApplicationMode.test
+#elseif DEBUG
+let mode = ApplicationMode.debug
+#else
+let mode = RunningMode.release
+#endif
