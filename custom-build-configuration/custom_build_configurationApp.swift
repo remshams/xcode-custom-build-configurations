f4dfe384ef034e50b1a0e3c6709c616c12a914c4
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-04-25 12:23:27 +0200

    * init

---
+++ custom-build-configuration/custom_build_configurationApp.swift
@@ -0,0 +1,17 @@
+//
+//  custom_build_configurationApp.swift
+//  custom-build-configuration
+//
+//  Created by Mathias Remshardt on 25.04.21.
+//
+
+import SwiftUI
+
+@main
+struct custom_build_configurationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ custom-build-configurationTests/custom_build_configurationTests.swift
@@ -0,0 +1,33 @@
+//
+//  custom_build_configurationTests.swift
+//  custom-build-configurationTests
+//
+//  Created by Mathias Remshardt on 25.04.21.
+//
+
+import XCTest
+@testable import custom_build_configuration
+
+class custom_build_configurationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
